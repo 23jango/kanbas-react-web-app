1dc58caea32e5f1a27df660cbc4b1a50de8838cf
Jayden Ngo <134519694+23jango@users.noreply.github.com>
2024-09-18 21:58:07 -0400

    Update index.tsx for deploy

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -155,7 +155,7 @@ export default function Lab1() {
         <br />
         Loading a local image:
         <br />
-        <img id="wd-teslabot" src="images/pirate.png" height="200px" />
+        <img id="wd-teslabot" src="images/pirate.png" height="200px" alt="pirateimg" />
       </div>
 
       <div id="wd-forms">
